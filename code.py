d6a7c914a94cdb07da5fb75e1d8b35c37b65cd72
7a7o77 <112498970+7a7o77@users.noreply.github.com>
2022-08-31 12:48:09 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,33 @@
 from time import sleep
 import requests
 import json
-import webbrowser
 import os
 try:
+    users = []
     attempts = 0
-    data = {"authorization":"AUTHCODE"} #enter your auth code here
+    data = {"authorization":"auth code"}
+    url = "https://kessel-api.parsec.app/v2/hosts?mode=game&public=true"
     count = 0
     peer = 0
+    def connect(peer):
+        print("[system]peer:"+peer+"\n[system]user:"+i["user"]["name"]+"\n[system]lobbyname:"+i["name"])
+        sleep(1)
+        os.system('C:/"Program Files"/Parsec/parsecd.exe peer_id='+peer)
+        print("quiting in 5 seconds")
+        input("[try again ?]\n[doesnt join the lobbies you already joined]\nenter?")
+        print("looking for open lobbies...")
+    def filter(i,solo,gamename):
+        if solo.lower() == "y" or solo.lower() == "yes" :
+            if gamename.lower().replace(" ","") in i["name"].lower().replace(" ","") and i["players"] == 0 and not i["user"]["id"] in users:
+                peer = i["peer_id"]
+                users.append(i["user"]["id"])
+                connect(peer)
+        else:
+            if gamename.lower().replace(" ","") in i["name"].lower().replace(" ","") and i["players"] < i["max_players"] and not i["user"]["id"] in users:
+                print("found")
+                peer = i["peer_id"]
+                users.append(i["user"]["id"])
+                connect(peer)
     gamename = input("enter the game name\ngame:")
     solo = input("do you want to play game with the host alone?\ny/n:")
     print("looking for open lobbies...")
@@ -17,30 +37,9 @@ try:
             r.headers.update(data)
             t = r.get(url)
             list = json.loads(t.text)
-            if solo.lower() == "y" or solo.lower() == "yes":
-                for i in list["data"]:
-                    if gamename.lower() in i["name"].lower() and i["players"] == 0:
-                        peer = i["peer_id"]
-                        print("[system]peer:"+peer+"\n[system]user:"+i["user"]["name"]+"\n[system]lobbyname:"+i["name"])
-                        sleep(1)
-                        os.system('C:/"Program Files"/Parsec/parsecd.exe peer_id='+peer)
-                        print("quiting in 5 seconds")
-                        sleep(5)
-                        quit()
-            else:
-                for i in list["data"]:
-                    if gamename.lower() in i["name"].lower() and i["players"] < i["max_players"]:
-                        peer = i["peer_id"]
-                        print("[system]peer:"+peer+"\n[system]user:"+i["user"]["name"]+"\n[system]lobby_name:"+i["name"])
-                        sleep(1)
-                        os.system('C:/"Program Files"/Parsec/parsecd.exe peer_id='+peer)
-                        print("quiting in 5 seconds")
-                        sleep(5)
-                        quit()
-            print(f"no lobbies refreshing...\nattempts:{attempts}")
-            attempts = attempts + 1
-            
-
+            for i in list["data"]:
+                filter(i,solo,gamename)
+                
         except KeyError:
             print("wrong auth code")
             sleep(3)
